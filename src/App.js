b237874880fe2712a0526cadcc5349d922685a5d
@imadnouali01 <imadnouali01@gmail.com>
2020-12-14 17:25:06 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import Repositories from "./components/Repositories/Repositories";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Github repos </h1>
+    <div className="d-flex flex-column justify-content-center container mt-5">
+      <Repositories />
     </div>
   );
 }
